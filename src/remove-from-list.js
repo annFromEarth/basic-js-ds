2fc9cf5db1a2242248884bdb1f92054eb0ebb1ad
annFromEarth <dv_anna@mail.ru>
2023-04-14 19:18:24 +0300

    remove-from-list v1

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,10 +22,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+
+  let current = l;
+  let prev = null;
+
+  while (current) {
+    if (current.value === k) {
+      if (prev) {
+        prev.next = current.next;
+      } else {
+        current.value = null;
+        current.next = null;
+      }
+    }
+    if (current.value !== k) {
+      // console.log(current.value);
+      prev = current;
+    }
+
+    current = current.next;
+  }
+  // console.log(l);
+  return l;
 }
+
+
+      // return l.next;
+
+    // if (list[i].value === k) {
+    //   list[i] 
+    // }
+  
+
+
 
 module.exports = {
   removeKFromList
